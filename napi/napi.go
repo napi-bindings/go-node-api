f58e808a9feacd03113ad54c30de7a443b7b5f54
NickNaso <nicoladelgobbo@gmail.com>
2021-03-26 20:04:51 +0100

    Added basic binding to napi_env.

--- napi/napi.go
+++ napi/napi.go
@@ -15,7 +15,30 @@ package napi
 #include <node_api.h>
 */
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+// ccstring function transforms a Go string into a C string (array of characters)
+// and returns the pointer to the first element.
+func cstring(s string) unsafe.Pointer {
+	p := make([]byte, len(s)+1)
+	copy(p, s)
+	return unsafe.Pointer(&p[0])
+}
+
+type napi_env = C.napi_env
+
+// Env is used to represent a context that the underlying Node-API
+// implementation can use to persist VM-specific state. This structure is
+// passed to native functions when they're invoked, and it must be passed back
+// when making Node-API calls. Specifically, the same Env that was passed in
+// when the initial native function was called must be passed to any subsequent
+// nested Node-API calls.
+type Env struct{}
+
+// TODO define fields and methods for Env
 
 // Hello1
 func Hello1() {
