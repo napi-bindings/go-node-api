75bb9e4e4bc2e3e4e462c0d9b6a143dab85b2974
NickNaso <nicoladelgobbo@gmail.com>
2021-03-26 17:20:34 +0100

    Added indirection system to call in C code.

--- napi/gonapi.cc
+++ napi/gonapi.cc
@@ -2,6 +2,86 @@
 
 #include "_cgo_export.h"
 
+struct CallbackWrap {
+  CallbackWrap(void* data) : data{data} {}
+  napi_callback operator()() {
+    static auto dataCopy = data;
+    return [](napi_env env, napi_callback_info info) -> napi_value {
+        return CallCallback(dataCopy, env, info);
+    };
+  }
+  void* data;
+};
+
+struct AsyncExecuteCallbackWrap {
+  AsyncExecuteCallbackWrap(void* data) : data{data} {}
+  napi_async_execute_callback operator()() {
+    static auto dataCopy = data;
+    return [](napi_env env, void* data) -> void {
+        return CallAsyncExecuteCallback(dataCopy, env, data);
+    };
+  }
+  void* data;
+};
+
+struct AsyncCompleteCallbackWrap {
+  AsyncCompleteCallbackWrap(void* data) : data{data} {}
+  napi_async_complete_callback operator()() {
+    static auto dataCopy = data;
+    return [](napi_env env, napi_status status, void* data) -> void {
+        return CallAsyncCompleteCallback(dataCopy, env, status, data);
+    };
+  }
+  void* data;
+};
+
+struct FinalizeCallbackWrap {
+  FinalizeCallbackWrap(void* data) : data{data} {}
+  napi_finalize operator()() {
+    static auto dataCopy = data;
+    return [](napi_env env, void* data, void* hint) -> void {
+        return CallFinalizeCallback(dataCopy, env, data, hint);
+    };
+  }
+  void* data;
+};
+
+struct ThreadsafeFunctionCallbackWrap {
+  ThreadsafeFunctionCallbackWrap(void* data) : data{data} {}
+  napi_threadsafe_function_call_js operator()() {
+    static auto dataCopy = data;
+    return [](napi_env env, napi_value callback, void* ctx, void* data) -> void {
+        return CallThreadsafeFunctionCallback(dataCopy, env, callback, ctx, data);
+    };
+  }
+  void* data;
+};
+
 extern "C" {
+napi_callback Callback(void* caller) {
+  CallbackWrap cb{caller};
+  return cb();
+}
+
+napi_async_execute_callback AsyncExecuteCallback(void* caller) {
+  AsyncExecuteCallbackWrap cb{caller};
+  return cb();
+
+}
+
+napi_async_complete_callback AsyncCompleteCallback(void* caller) {
+  AsyncCompleteCallbackWrap cb{caller};
+  return cb();
+}
+
+napi_finalize FinalizeCallback(void* caller) {
+  FinalizeCallbackWrap cb{caller};
+  return cb();
+}
+
+napi_threadsafe_function_call_js ThreadsafeFunctionCallback(void* caller) {
+  ThreadsafeFunctionCallbackWrap cb{caller};
+  return cb();
+}
 
 }
--- napi/gonapi.h
+++ napi/gonapi.h
@@ -11,4 +11,25 @@ extern "C" {
 }  // extern "C"
 #endif
 
+#ifndef GO_NAPI_H
+#define GO_NAPI_H
+
+#include <node_api.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern napi_callback Callback(void* caller);
+extern napi_async_execute_callback AsyncExecuteCallback(void* caller);
+extern napi_async_complete_callback AsyncCompleteCallback(void* caller);
+extern napi_finalize FinalizeCallback(void* caller);
+extern napi_threadsafe_function_call_js ThreadsafeFunctionCallback(void* caller);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // GO_NAPI_H
+
 #endif  // GO_NAPI_H
